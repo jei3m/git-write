11cae8b712ad79d6417688d1a764a12a6f9b8567
jei3m <virozzegt@gmail.com>
2025-05-05 14:03:07 +0800

    style: implemented themes at tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sidebar: {
+          background: 'hsl(var(--sidebar-background))',
+          foreground: 'hsl(var(--sidebar-foreground))',
+          primary: 'hsl(var(--sidebar-primary))',
+          'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+          accent: 'hsl(var(--sidebar-accent))',
+          'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+          border: 'hsl(var(--sidebar-border))',
+          ring: 'hsl(var(--sidebar-ring))'
+        }
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
