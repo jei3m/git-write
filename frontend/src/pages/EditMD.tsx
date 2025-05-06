21fff41afa82617cc6ddfed6b48c232e67add93d
jei3m <virozzegt@gmail.com>
2025-05-06 09:22:54 +0800

    feat: implemented select feature and select repos component

--- frontend/src/pages/EditMD.tsx
+++ frontend/src/pages/EditMD.tsx
@@ -1,21 +1,42 @@
 import {useState, useEffect} from 'react'
 import MarkdownEditor from '@uiw/react-markdown-editor';
 import { useTheme } from '../contexts/ThemeProvider';
-import { DownloadIcon } from 'lucide-react';
+import { CircleXIcon, DownloadIcon } from 'lucide-react';
 import TemplateSelector from '@/components/edit/TemplateSelector';
 import { Button } from '@/components/ui/button';
+import SelectRepos from '@/components/edit/SelectRepos';
+import SelectFeature from '@/components/edit/SelectFeature';
+import { useRepoStore } from "@/store/repo.store";
+import { toast } from 'sonner';
 
 function Edit() {
   const { theme } = useTheme();
   const [markdown, setMarkdown] = useState("")
+  const [selectedFeature, setSelectedFeature] = useState("")
+  const [selectedRepo, setSelectedRepo] = useState<string | null>(null);
+  const [repoName, setRepoName] = useState("");
 
   const handleDownload = () => {
+
+    if (markdown === ""){
+      toast.error('Your README is empty.');
+      return;
+    }
+
     const element = document.createElement('a');
     const file = new Blob([markdown], {type: 'text/plain'});
     element.href = URL.createObjectURL(file);
     element.download = 'README.md';
     document.body.appendChild(element);
     element.click();
+  }
+
+  const handleClearFeature = () => {
+    setSelectedFeature("")
+    setMarkdown("")
+    setSelectedRepo("")
+    setRepoName("")
+    useRepoStore.setState({ readme: "" })
   }
 
   useEffect(() => {
@@ -28,9 +49,40 @@ function Edit() {
         <div className='flex flex-row justify-between'>
           <h1 className="text-3xl font-bold mb-4 text-black dark:text-white">Markdown Editor</h1>
           <div className='flex flex-row gap-4'>
-            <TemplateSelector setMarkdown={setMarkdown} />
-            <Button className='h-[36px] bg-white dark:bg-gray-900 border border-gray-300 dark:border-neutral-700 text-black dark:text-white' onClick={handleDownload}><DownloadIcon/>Download</Button>
-            {/* <Button className='h-[36px] bg-white dark:bg-gray-900 border border-gray-300 dark:border-neutral-700 text-black dark:text-white'><Github/>Publish</Button> */}
+
+            {!selectedFeature ? (
+              <div className='flex flex-row gap-x-3'>
+                <SelectFeature setSelectedFeature={setSelectedFeature} selectedFeature={selectedFeature} />
+              </div>
+            ) :(
+              <Button className='h-[36px] bg-white dark:bg-gray-900 border border-gray-300 dark:border-neutral-700 text-black dark:text-white'
+                onClick={handleClearFeature}
+              >
+                <CircleXIcon/>
+              </Button>
+            )}
+
+            {selectedFeature === "repos" && 
+              <SelectRepos 
+                selectedRepo={selectedRepo}
+                setSelectedRepo={setSelectedRepo}
+                repoName={repoName}
+                setRepoName={setRepoName}
+                setMarkdown={setMarkdown}
+              />
+            }
+
+            {selectedFeature === "templates" && 
+              <TemplateSelector setMarkdown={setMarkdown} /> 
+            }
+
+            <Button 
+              className='h-[36px] bg-white dark:bg-gray-900 border border-gray-300 dark:border-neutral-700 text-black dark:text-white' 
+              onClick={handleDownload}
+            >
+              <DownloadIcon/>Download
+            </Button>
+
           </div>
         </div>
         <MarkdownEditor
