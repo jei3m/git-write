e5a1e3132657fd51dd12f6c0bbf027e6a6b6311c
jei3m <virozzegt@gmail.com>
2025-05-06 09:20:40 +0800

    feat: select feature component

---
+++ frontend/src/components/edit/SelectFeature.tsx
@@ -0,0 +1,31 @@
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+
+function SelectFeature({setSelectedFeature, selectedFeature}: any) {
+
+    const handleSelectTemplate = (value: string) => {
+        setSelectedFeature(value);
+    };
+
+  return (
+    <Select onValueChange={handleSelectTemplate} value={selectedFeature}>
+      <SelectTrigger className="w-[220px] bg-white dark:bg-gray-900 border-gray-300 dark:border-neutral-700 text-black dark:text-white">
+        <SelectValue placeholder="Select Feature" />
+      </SelectTrigger>
+      <SelectContent className='bg-white dark:bg-gray-900 border-gray-300 dark:border-neutral-700 focus-visible:ring-0'>
+        <SelectGroup className='text-black dark:text-white'>
+          <SelectItem value="repos">Repository</SelectItem>
+          <SelectItem value="templates">Template</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  )
+}
+
+export default SelectFeature
