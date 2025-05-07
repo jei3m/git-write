f004cfbf07ad0a2a4b8e683079b060da0e09f448
jei3m <virozzegt@gmail.com>
2025-05-07 14:51:31 +0800

    ref: renamed repo selector component

--- frontend/src/components/edit/RepoSelector.tsx
+++ frontend/src/components/edit/RepoSelector.tsx
@@ -60,7 +60,7 @@ function SelectRepos({selectedRepo, setSelectedRepo, setRepoName, setMarkdown}: 
                             placeholder="Search repositories..."
                             value={searchQuery}
                             onChange={(e) => setSearchQuery(e.target.value)}
-                            className="h-8 flex-1 bg-transparent text-black dark:text-white focus-visible:ring-0"
+                            className="h-8 flex-1 bg-transparent text-black dark:text-white border-gray-300 dark:border-neutral-700 focus-visible:ring-0"
                         />
                     </div>
                     <ScrollArea className="h-[200px]">
