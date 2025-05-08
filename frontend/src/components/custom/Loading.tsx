b54f50ab90eafa9edc1fb161c572f7af818aee85
jei3m <virozzegt@gmail.com>
2025-05-08 21:46:26 +0800

    style: maximized height of loading component

--- frontend/src/components/custom/Loading.tsx
+++ frontend/src/components/custom/Loading.tsx
@@ -1,6 +1,6 @@
 function Loading() {
   return (
-    <div className="min-h-[90dvh] flex items-center justify-center">
+    <div className="min-h-[100dvh] flex items-center justify-center">
       <div className="h-12 w-12 animate-spin rounded-full border-[6px] border-solid border-black dark:border-white border-t-transparent dark:border-primary-foreground dark:border-t-transparent" aria-label="loading"></div>
     </div>
   )
