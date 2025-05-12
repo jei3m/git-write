0c2ee644906cf11b32fb7dd67c30e5f6706bb892
jei3m <virozzegt@gmail.com>
2025-05-12 20:42:43 +0800

    ref: migrated Authentication from Auth0 to Firebase

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,35 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './Router.tsx'
-import { Auth0Provider } from '@auth0/auth0-react';
 import { ThemeProvider } from './contexts/ThemeProvider.tsx'
 import { Toaster } from "@/components/ui/sonner.tsx"
-
-function Root() {
-  const domain = import.meta.env.VITE_AUTH_DOMAIN;
-  const clientId = import.meta.env.VITE_AUTH_CLIENT_ID;
-  const redirectUri = import.meta.env.VITE_REDIRECT_URI;
-
-
-  return (
-    <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      authorizationParams={{
-        redirect_uri: redirectUri,
-      }}
-    >
-      <App />
-      <Toaster richColors position="bottom-right" theme='light' expand={true} />
-      
-    </Auth0Provider>
-  )
-}
+import { AuthProvider } from './contexts/FirebaseContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <Root />
+      <AuthProvider>
+        <App />
+        <Toaster richColors position="bottom-right" theme='light' expand={true} />
+      </AuthProvider>
     </ThemeProvider>
   </StrictMode>,
 )
