05dcc413251b723144ea5ee74d31cde857e8c7af
jei3m <virozzegt@gmail.com>
2025-05-12 14:08:53 +0800

    feat: store function for fetch sha from github api

--- frontend/src/store/github.store.ts
+++ frontend/src/store/github.store.ts
@@ -2,10 +2,13 @@ import { create } from "zustand";
 import {RepoStore} from "@/types/github.types";
 import axios from "axios";
 
+const GITWRITE_AUTH_TOKEN = import.meta.env.VITE_GITWRITE_AUTH_TOKEN;
+
 export const useGithubStore = create<RepoStore>((set) => ({
     readme: "",
     repos: [],
     gitUser: null,
+    sha: null,
     setRepos: (repos) => set({repos}),
     fetchRepos: async (githubUsername?: string) => {
         try {
@@ -40,6 +43,19 @@ export const useGithubStore = create<RepoStore>((set) => ({
             return {success: true, message: "User data fetched successfully"};
         } catch (error) {
             return {success: false, message: "Failed to fetch user data"};
+        }
+    },
+    fetchSHA: async (full_name: string) => {
+        try {
+            const { data } = await axios.get(`https://api.github.com/repos/${full_name}/contents/README.md`, {
+                headers: {
+                    'Authorization': GITWRITE_AUTH_TOKEN
+                }
+            })
+            set({sha: data.sha});
+            return {success: true, message: "SHA fetched successfully"};
+        } catch (error) {
+            return {success: false, message: "Failed to fetch SHA"};
         }
     }
 }));
