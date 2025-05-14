4fdcc14553ec1eb7834f90f00090580e3b7f4558
jei3m <virozzegt@gmail.com>
2025-05-14 23:41:10 +0800

    feat: implement error handling and loading state

--- frontend/src/components/edit/TemplateSelector.tsx
+++ frontend/src/components/edit/TemplateSelector.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { UserAuth } from '@/contexts/FirebaseContext';
 import { useTemplateStore } from '@/store/template.store';
 import { Button } from "@/components/ui/button";
-import { CircleX, SquarePen, PlusIcon, Search } from "lucide-react";
+import { CircleX, SquarePen, PlusIcon, Search, Loader2 } from "lucide-react";
 import DeleteDialog from './DeleteDialog';
 import { toast } from "sonner";
 import { TemplateSelectorProps } from '@/types/template.types';
@@ -20,11 +20,24 @@ function TemplateSelector({setMarkdown}: TemplateSelectorProps) {
     const [selectedTemplate, setSelectedTemplate] = useState("");
     const [open, setOpen] = useState(false);
     const [searchQuery, setSearchQuery] = useState("");
+    const [isLoading, setIsLoading] = useState(true);
     
     useEffect(() => {
-        if (userId) {
-            fetchTemplates(userId);
-        }
+        const loadTemplates = async () => {
+            try {
+                const {success, message } = await fetchTemplates(userId);
+                if (!success) {
+                    toast.error(message);
+                }
+            } catch (error) {
+                setIsLoading(false);
+                console.error("Error loading templates:", error);
+                toast.error("Error loading templates");
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        loadTemplates();
     }, [userId]);
 
     const sortedTemplates = [...templates].sort((a, b) => {
@@ -79,63 +92,67 @@ function TemplateSelector({setMarkdown}: TemplateSelectorProps) {
                 </Button>
             </PopoverTrigger>
             <PopoverContent className="w-[240px] lg:w-[340px] p-0 bg-white dark:bg-gray-900 border-gray-300 dark:border-neutral-700">
-                <div className="p-2">
-                    <div className="flex items-center space-x-2 mb-2">
-                        <Search className="h-4 w-4 text-gray-500 dark:text-gray-400" />
-                        <Input
-                            placeholder="Search templates..."
-                            value={searchQuery}
-                            onChange={(e) => setSearchQuery(e.target.value)}
-                            className="h-8 flex-1 bg-transparent text-black dark:text-white border-gray-300 dark:border-neutral-700 focus-visible:ring-0"
-                        />
-                    </div>
-                    <ScrollArea className="max-h-[200px]">
-                        <div className="space-y-1 text-black dark:text-white">
-                            {filteredTemplates.map((template) => (
-                                <div className="flex items-center justify-between" key={template._id}>
-                                    <Button
-                                        variant="ghost"
-                                        className="flex-1 justify-start font-normal truncate"
-                                        onClick={() => handleSelectTemplate(template._id)}
-                                    >
-                                        {template.title}
-                                    </Button>
-                                    <div className="flex flex-row gap-x-1">
-                                        <DeleteDialog handleDeleteTemplate={handleDeleteTemplate} template={template} />
-                                        <Button 
-                                            variant="ghost" 
-                                            size="icon" 
-                                            className="h-8 w-8" 
-                                            onClick={() => handleUpdateTemplate(template._id)}
-                                        >
-                                            <SquarePen className="h-4 w-4 mt-[5px]" />
-                                        </Button>
-                                    </div>
-                                </div>
-                            ))}
-                            {filteredTemplates.length === 0 && <div className='p-2 text-sm'>No templates found</div>}
+                {isLoading ? (
+                    <div className='p-2 text-center flex justify-center items-center'><Loader2 className='animate-spin text-sm text-black dark:text-white'/></div>
+                ): (
+                    <div className="p-2">
+                        <div className="flex items-center space-x-2 mb-2">
+                            <Search className="h-4 w-4 text-gray-500 dark:text-gray-400" />
+                            <Input
+                                placeholder="Search templates..."
+                                value={searchQuery}
+                                onChange={(e) => setSearchQuery(e.target.value)}
+                                className="h-8 flex-1 bg-transparent text-black dark:text-white border-gray-300 dark:border-neutral-700 focus-visible:ring-0"
+                            />
                         </div>
-                    </ScrollArea>
-                    <div className="mt-2 flex flex-row gap-x-2">
-                        {selectedTemplate && (
-                            <Button 
-                                variant="outline"
-                                className="text-red-500 dark:text-red-400 justify-center bg-gray-100 dark:bg-gray-800 text-xs"
-                                onClick={handleClearTemplate}
-                            >
-                                <CircleX className="-mr-1 h-4 w-4"/>Unselect template
-                            </Button>
-                        )}
-                        <Link to={'/create'} className="w-full">
-                            <Button 
-                                variant="outline"
-                                className="text-black dark:text-white w-full justify-center bg-gray-100 dark:bg-gray-800 text-xs"
-                            >
-                                <PlusIcon className="-mr-1 h-4 w-4"/>Create template
-                            </Button>
-                        </Link>
+                        <ScrollArea className="max-h-[200px]">
+                            <div className="space-y-1 text-black dark:text-white">
+                                {filteredTemplates.map((template) => (
+                                    <div className="flex items-center justify-between" key={template._id}>
+                                        <Button
+                                            variant="ghost"
+                                            className="flex-1 justify-start font-normal truncate"
+                                            onClick={() => handleSelectTemplate(template._id)}
+                                        >
+                                            {template.title}
+                                        </Button>
+                                        <div className="flex flex-row gap-x-1">
+                                            <DeleteDialog handleDeleteTemplate={handleDeleteTemplate} template={template} />
+                                            <Button 
+                                                variant="ghost" 
+                                                size="icon" 
+                                                className="h-8 w-8" 
+                                                onClick={() => handleUpdateTemplate(template._id)}
+                                            >
+                                                <SquarePen className="h-4 w-4 mt-[5px]" />
+                                            </Button>
+                                        </div>
+                                    </div>
+                                ))}
+                                {filteredTemplates.length === 0 && <div className='p-2 text-sm'>No templates found</div>}
+                            </div>
+                        </ScrollArea>
+                        <div className="mt-2 flex flex-row gap-x-2">
+                            {selectedTemplate && (
+                                <Button 
+                                    variant="outline"
+                                    className="text-red-500 dark:text-red-400 justify-center bg-gray-100 dark:bg-gray-800 text-xs"
+                                    onClick={handleClearTemplate}
+                                >
+                                    <CircleX className="-mr-1 h-4 w-4"/>Unselect template
+                                </Button>
+                            )}
+                            <Link to={'/create'} className="w-full">
+                                <Button 
+                                    variant="outline"
+                                    className="text-black dark:text-white w-full justify-center bg-gray-100 dark:bg-gray-800 text-xs"
+                                >
+                                    <PlusIcon className="-mr-1 h-4 w-4"/>Create template
+                                </Button>
+                            </Link>
+                        </div>
                     </div>
-                </div>
+                )}
             </PopoverContent>
         </Popover>
     );
