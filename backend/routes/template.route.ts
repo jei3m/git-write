0299ecf5852679725196908b0dc11b23fc000e33
jei3m <virozzegt@gmail.com>
2025-05-18 23:47:50 +0800

    ref: updated template routes

--- backend/routes/template.route.ts
+++ backend/routes/template.route.ts
@@ -1,12 +1,11 @@
-import express, {Router} from "express";
+import express from "express";
 import { createTemplate, getTemplates, getTemplateById, updateTemplate, deleteTemplate } from "../controllers/template.controller";
+import { secretKeyMiddleware } from "../middleware/secret-key.middleware";
 
-const router: Router = express.Router();
+const router = express.Router();
 
-// since you can only have one route parameter, use query parameters for non-unique identifiers as filter
-router.get('/', getTemplates);
-// You only can have one get/:id route (route parameter).
-router.get('/:id', getTemplateById); 
+router.get('/:userId', getTemplates);
+router.get('/:userId/:id', getTemplateById); 
 router.post('/', createTemplate);
 router.put('/:id', updateTemplate);
 router.delete('/:id', deleteTemplate);
