7dd01ced8d69a384e4e15a5019c9811c1bf4e84b
jei3m <virozzegt@gmail.com>
2025-05-18 23:47:18 +0800

    feat: implement secret key middleware

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -2,7 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "../config/db";
 import templateRoutes from "../routes/template.route";
+import testRoutes from "../routes/test.route";
 import cors from "cors";
+import { secretKeyMiddleware } from "../middleware/secret-key.middleware";
 
 dotenv.config();
 
@@ -15,10 +17,11 @@ app.use(cors({origin: process.env.ORIGIN_URL})); // Only allow requests from thi
 app.use(express.json()); // To accept json data in req.body
 
 app.get("/", (req, res) => {
-    res.send('API is Running!')
+    res.send('API is Running! 🎉')
 });
 
-app.use("/api/templates", templateRoutes);
+app.use("/api/tests", testRoutes);
+app.use("/api/templates", secretKeyMiddleware, templateRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`);
