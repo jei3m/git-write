1bc77cd88788031d7f70ff513bcc0c40fcc2f3ec
jei3m <virozzegt@gmail.com>
2025-05-21 00:18:01 +0800

    Revert "feat: custom encrypt and decryp utils"
    This reverts commit 126f29bd8f463e36262ecedceea728b4807a9766.

--- frontend/src/utils/base64.ts
+++
@@ -1,7 +0,0 @@
-export function toBase64(str: string) {
-    return btoa(String.fromCharCode(...new Uint8Array(new TextEncoder().encode(str))));
-}
-
-export function fromBase64(str: string) {
-    return atob(String.fromCharCode(...new Uint8Array(new TextEncoder().encode(str))));
-}
