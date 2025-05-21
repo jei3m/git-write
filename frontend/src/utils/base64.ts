4a5bfa0af2882da52d55062a6f25eca11a9ab3a6
jei3m <virozzegt@gmail.com>
2025-05-22 01:28:47 +0800

    fix:(#11) updated base64 to string conversion to support emojis

--- frontend/src/utils/base64.ts
+++ frontend/src/utils/base64.ts
@@ -3,5 +3,10 @@ export function toBase64(str: string) {
 }
 
 export function fromBase64(str: string) {
-    return atob(String.fromCharCode(...new Uint8Array(new TextEncoder().encode(str))));
+    const binaryString = atob(str);
+    const bytes = new Uint8Array(binaryString.length);
+    for (let i = 0; i < binaryString.length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+    return new TextDecoder().decode(bytes);
 }
