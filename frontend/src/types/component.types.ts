d0b19042c3e2b9bd4307183fa4ac9fc80664483d
jei3m <virozzegt@gmail.com>
2025-05-24 11:57:07 +0800

    feat: custom component types

---
+++ frontend/src/types/component.types.ts
@@ -0,0 +1,21 @@
+export interface SelectRepoProps {
+    selectedRepo: string;
+    setSelectedRepo: (repoTitle: string) => void;
+    setRepoFullName: (repoName: string) => void;
+    setInitialReadme: (readme: string) => void;
+    setMarkdown: (readme: string) => void;
+}
+
+export interface SelectFeatureProps {
+    selectedFeature: string;
+    setSelectedFeature: (value: string) => void;
+}
+
+export interface TemplateSelectorProps {
+    setMarkdown: (value: string) => void;
+}
+
+export interface DeleteTemplateProps {
+    handleDeleteTemplate: (id: string) => void;
+    template: {};
+}
