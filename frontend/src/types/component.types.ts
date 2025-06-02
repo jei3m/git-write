fa125f478e1619e2e320070d05c67b4ea51a7dd4
jei3m <virozzegt@gmail.com>
2025-06-03 05:24:49 +0800

    ref: remove prop types for unused prop

--- frontend/src/types/component.types.ts
+++ frontend/src/types/component.types.ts
@@ -1,17 +1,9 @@
-import { Dispatch, SetStateAction } from "react";
-
 export interface SelectRepoProps {
     selectedRepo: string | null;
     setSelectedRepo: (repoTitle: string) => void;
     setRepoFullName: (repoName: string) => void;
     setInitialReadme: (readme: string) => void;
     setMarkdown: (readme: string) => void;
-    setCommit: Dispatch<SetStateAction<{
-        full_name: string;
-        sha: string;
-        content: string;
-        message: string;
-    }>>;
 }
 
 export interface SelectFeatureProps {
